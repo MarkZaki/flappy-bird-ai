d86eca40cade4028736b79682a1263caa0f17a93
Mark Zaky <markzaky18@gmail.com>
2020-06-30 13:27:48 +0200

    Finished The Graphics

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import time
 import os
 import random
 
+pygame.font.init()
+
 WIN_HEIGHT = 700
-WIN_WIDTH = 570
+WIN_WIDTH = 550
+
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
 
 BIRD_IMG = [
     pygame.transform.scale2x(pygame.image.load(
@@ -163,25 +167,63 @@ class Base:
     win.blit(self.IMG, (self.x2, self.y))
 
 
-def draw_window(win, bird):
-  win.blit(BG_IMG, (0, 0))
+def draw_window(win, bird, pipes, base, score):
+  win.blit(BG_IMG, (0, -200))
+  for pipe in pipes:
+    pipe.draw(win)
+  txt = STAT_FONT.render("Score: " + str(score), 1, (255, 255, 255))
+  win.blit(txt, (WIN_WIDTH - 10 - txt.get_width(), 10))
+  base.draw(win)
   bird.draw(win)
   pygame.display.update()
 
 
 def main():
   pygame.init()
-  bird = Bird(200, 200)
+  bird = Bird(230, 350)
+  base = Base(630)
+  pipes = [Pipe(600)]
   win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
   clock = pygame.time.Clock()
+  score = 0
   run = True
+
   while run:
     clock.tick(30)
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         run = False
+
+    add_pipe = False
+    rem = []
+    for pipe in pipes:
+
+      if pipe.collide(bird):
+        print(1)
+      else:
+        print(0)
+
+      if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+        rem.append(pipe)
+
+      if not pipe.passed and pipe.x < bird.x:
+        pipe.passed = True
+        add_pipe = True
+
+      pipe.move()
+
+    if add_pipe:
+      score += 1
+      pipes.append(Pipe(600))
+
+    for r in rem:
+      pipes.remove(r)
+
+    if bird.y + bird.img.get_height() >= 630:
+      pass
+
     bird.move()
-    draw_window(win, bird)
+    draw_window(win, bird, pipes, base, score)
   pygame.quit()
   quit()
 
